96c115746517517a23d9cd598bef9eef0e8cdf72
Ana Ignat <anamaria.marchis@gmail.com>
2025-02-28 12:23:59 +0200

    Adding Day 2

---
+++ Day2/index.js
@@ -0,0 +1,38 @@
+console.log("Day 02");
+
+// Declaring and assigning variables
+
+const personName = "Anna";
+let personAge;
+personAge = 38
+let personFavoriteProgLang = "JavaScript";
+let personIsStudent = true;
+
+console.log(`${personName} is ${personAge} years old, likes ${personFavoriteProgLang} and is ${personIsStudent} that she is a student.`);
+
+// Playong with variables
+
+personAge = 30;
+personFavoriteProgLang = "HTML";
+personIsStudent = false;
+
+console.log(`${personName} is ${personAge} years old, likes ${personFavoriteProgLang} and is ${personIsStudent} that she is a student.`);
+
+
+// Object
+
+let person = {
+    name: "Anna",
+    age: 38,
+    favoriteProgLang: "JavaScript",
+    isStudent: true
+}
+
+console.log(person);
+
+// Arrays
+
+let fruits = ["bananas", "apples", "mangoes", "kiwis"];
+fruits = 2;
+
+console.log(fruits);
